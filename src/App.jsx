8c6f92af9b7f21ef763192a0f6b30d395b854766
cheesecodestudio <cheesecodestudio@gmail.com>
2024-04-23 19:33:18 -0600

    init project and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <header><h1 class="text-3xl font-bold underline text-green-700">Header</h1></header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
